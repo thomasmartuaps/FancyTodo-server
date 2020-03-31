8945d1352d3911527d560e2d113fb29f42e00d8b
thomasmartuaps <thomasmartuaps@gmail.com>
2020-03-31 08:14:25 +0700

    Update still in progress

--- controllers/todo.js
+++ controllers/todo.js
@@ -10,7 +10,47 @@ class TodoController {
             UserId: req.decoded.id || 1
         }
         Todo.create(newTask)
-            .then()
+            .then(response => {
+                return res.status(201).json(response)
+            })
+            .catch(err => {
+                return res.status(500).json(err)
+            })
+    }
+    static read(req, res) {
+        Todo.findAll({ where: { UserId: req.decoded.id || 1 }})
+            .then(response => {
+                return res.status(200).json(response)
+            })
+            .catch(err => {
+                return res.status(500).json(err)
+            })
+    }
+    static readById(req, res) {
+        Todo.findOne({ where: { id: req.params.id }})
+            .then(response => {
+                if(!response) {
+                    return res.status(404).json({ type: '404 Not found', msg: 'Item not found!'})
+                }
+                else {
+                    return res.status(200).json(response)
+                }
+            })
+            .catch(err => {
+                return res.status(500).json(err)
+            })
+    }
+    static update(req, res) {
+        let task = {
+            title: req.body.title,
+            description: req.body.description,
+            status: req.body.status,
+            due_date: req.body.due_date
+        }
+        Todo.update(task, { where: { id: req.params.id }, returning: true })
+            .then(response => {
+                return res.status(200).json(respon)
+            })
     }
 }
 

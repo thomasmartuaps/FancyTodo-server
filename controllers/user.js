5a6b4eb7926ae6055d3c0f0aac7cdc73772dc294
thomasmartuaps <thomasmartuaps@gmail.com>
2020-04-04 15:07:26 +0700

    Server update 1

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,9 @@
 const { User } = require('../models')
 const { createToken } = require('../helpers/jwt')
 const { checkPassword } = require('../helpers/bcrypt')
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(process.env.CLIENT_ID);
+
 
 class UserController {
     static register(req, res, next) {
@@ -12,7 +15,12 @@ class UserController {
                     email: response.email
                 }
                 let token = createToken(payload)
-                return res.status(201).json({id: payload.id, email: payload.email, token: token})
+                return res.status(201).json({
+                    id: payload.id, 
+                    email: payload.email, 
+                    token: token,
+                    avatar: `https://api.adorable.io/avatars/200/${response.email}.png` 
+                })
             })
             .catch(err => {
                 return next(err)
@@ -46,6 +54,59 @@ class UserController {
             .catch(err => {
                 return next(err)
             })
+    }
+    static googleSignIn(req, res, next) {
+        let googleToken = req.headers.token
+        let newUser = {}
+        client.verifyIdToken({
+            idToken: googleToken,
+            audience: process.env.CLIENT_ID
+        }).then(ticket => {
+            let payload = ticket.getPayload()
+            let userId = payload['sub']
+            let userEmail = payload.email
+            newUser.email = payload.email
+            newUser.password = process.env.DEFAULT_PASS
+            // console.log({payload: payload})
+            return User.findOne({where: {email: userEmail}})
+        })
+        .then(response => {
+            if(response) {
+                let payload = {
+                    id: response.id,
+                    email: response.email
+                }
+                let token = createToken(payload)
+                // console.log('user existed')
+                return res.status(200).json({
+                    id: payload.id, 
+                    email: payload.email, 
+                    token: token, 
+                    avatar: `https://api.adorable.io/avatars/200/${response.email}.png` 
+                })
+            }
+            else {
+                // console.log(response)
+                // console.log('No such user')
+                // console.log(newUser)
+                return User.create(newUser)
+            }
+        })
+        .then(response => {
+            let payload = {
+                id: response.id,
+                email: response.email
+            }
+            let token = createToken(payload)
+            // console.log('User created through Google')
+            return res.status(201).json({
+                id: payload.id, 
+                email: payload.email, 
+                token: token, 
+                avatar: `https://api.adorable.io/avatars/200/${response.email}.png` 
+            })
+        })
+        .catch(err => next(err))
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const authorization = require('../middlewares/authorization')
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+router.post('/googleSignIn', UserController.googleSignIn)
 
 router.use(authentication)
 router.post('/todos', TodoController.create)
